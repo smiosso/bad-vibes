78e9de080d466c1b3b11358201adfbb2d322aa77
Samantha Tamiosso <samicolognese@gmail.com>
2024-10-23 16:47:09 +0200

    final changes

---
+++ index.js
@@ -0,0 +1,30 @@
+// This array stores the messages
+const messages = [
+    'Things could always take a turn for the worse.',
+    'It\’s not over until it gets worse.',
+    'Nothing is so bad that it can\’t get worse.',
+    'The only person who cannot doubt you is yourself.',
+    'Just when you think it can\'t get worse, it usually does.',
+    'Life\'s a rollercoaster, and the lows are often deeper than the highs.',
+    'The best way to avoid being surprised by a bad day is to ruin everything right in the morning.'
+];
+
+// This variable stores the button
+let button = document.getElementById('button');
+
+//This variable stores the space to the message
+let buttonMessage = document.getElementById('buttonMessage');
+
+
+
+// This function will choose a random message
+function getRandomMessage() {
+    const randomIndex = Math.floor(Math.random() * messages.length);
+    const yourMessage = messages[randomIndex];
+    button.innerHTML = 'I am not convinced. Try again.'
+    buttonMessage.innerHTML = yourMessage;
+    
+}
+
+// event listener
+button.onclick = getRandomMessage;
